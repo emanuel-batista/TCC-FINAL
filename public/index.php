7d8865bb415b584f70152b1e9a5a8ea8027c964e
Emanuel <emanuel.silva44@etec.sp.gov.br>
2022-06-08 13:28:16 -0300

    Primeiro commit no que eu espero que seja a versão final do TCC

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AP!TA - O melhor PORTAL</title>
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+
+    <div class="nav">
+        <a href="index.php" class='logo'><img src="../assets/apita.svg" alt="LOGO DO SITE" id='logo'>AP!TA</a>
+        <div class="links">
+            <a href="#" class='icon'><img src="../assets/HOME.svg" alt="HOME" id='icon'></a>
+            <a href="#" class='icon'><img src="../assets/ENTREVISTAS.svg" alt="HOME" id='icon'></a>
+            <a href="#" class='icon'><img src="../assets/NOTICIAS.svg" alt="HOME" id='icon'></a>
+            <a href="#" class='icon'><img src="../assets/LOGIN.svg" alt="HOME" id='icon'></a>
+        </div>
+        
+    </div>
+    //conectar usuario
+    
+
+
+</body>
+</html>
