7aca3133eeb9bd692cc1656d7f3a72d2e13e4e4f
Emanuel <emanuel.silva44@etec.sp.gov.br>
2022-06-08 14:25:05 -0300

    segundo commit Deus ajude

--- public/index.php
+++ public/index.php
@@ -10,18 +10,17 @@
 <body>
 
     <div class="nav">
-        <a href="index.php" class='logo'><img src="../assets/apita.svg" alt="LOGO DO SITE" id='logo'>AP!TA</a>
+        <a href="index.php" class='logo'><img src="../static/images/apita.svg" alt="LOGO DO SITE" id='logo'>AP!TA</a>
         <div class="links">
-            <a href="#" class='icon'><img src="../assets/HOME.svg" alt="HOME" id='icon'></a>
-            <a href="#" class='icon'><img src="../assets/ENTREVISTAS.svg" alt="HOME" id='icon'></a>
-            <a href="#" class='icon'><img src="../assets/NOTICIAS.svg" alt="HOME" id='icon'></a>
-            <a href="#" class='icon'><img src="../assets/LOGIN.svg" alt="HOME" id='icon'></a>
+            <a href="#" class='icon'><img src="../static/images/HOME.svg" alt="HOME" id='icon'></a>
+            <a href="#" class='icon'><img src="../static/images/ENTREVISTAS.svg" alt="HOME" id='icon'></a>
+            <a href="#" class='icon'><img src="../static/images//NOTICIAS.svg" alt="HOME" id='icon'></a>
+            <a href="#" class='icon'><img src="../static/images/LOGIN.svg" alt="HOME" id='icon'></a>
         </div>
         
     </div>
-    //conectar usuario
     
-
-
+    
+    
 </body>
 </html>
