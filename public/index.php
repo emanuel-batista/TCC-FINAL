1ab0fc4044603dcae122e6c60186ae6d00a783a2
leexavier18 <leticia.damim@gmail.com>
2022-06-10 08:09:45 -0300

    foi adicionado um card

--- public/index.php
+++ public/index.php
@@ -16,7 +16,21 @@
     <?php require_once(ROOT_PATH . '/public_functions.php') ?>
     <?php $posts = getPublishedPosts(); ?>    
 
-    <!-- oiiii -->
-    <h1>hello world</h1>
+    
+    <div class="card mb-3" style="max-width: 540px;"> <!-- card -->
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="neymar.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
 </body>
 </html>
