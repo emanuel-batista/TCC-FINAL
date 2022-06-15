0a3964ef203df23b1d064a45877cb2279860592e
leexavier18 <leticia.damim@gmail.com>
2022-06-15 13:41:17 -0300

    Commit arrumei essa porra

--- public/index.php
+++ public/index.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AP!TA - O melhor PORTAL</title>
     <?php require_once '../includes/config.php'; ?>
-    <link rel="stylesheet" href="../static/style/style.css">
+    <link rel="stylesheet" href="../static/style/style.css"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
+
 <body>
 
     <?php require_once(ROOT_PATH . '/navbar.php'); ?> 
@@ -17,7 +18,7 @@
     <?php require_once(ROOT_PATH . '/public_functions.php') ?>
     <?php $posts = getPublishedPosts(); ?>    
 
-    
+
   <div class="card mb-3" style="auto;"> <!-- card carrossel -->
   <div class="row g-0">
     <div class="col-md-4" style="auto;">
